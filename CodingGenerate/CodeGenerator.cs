0922fb1ff3f4526dedabe093c94113941c5cffd1
daniyal2224 <daniyal2224@gmail.com>
2021-12-31 11:04:06 +0330

    Add project files.

---
+++ CodingGenerate/CodeGenerator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Emit;
+
+namespace CodeAnalyzer
+{
+    public static class CodeGenerator
+    {
+        private static ArgumentListSyntax CreateSingleArgument(string value)
+        {
+            return SyntaxFactory.ArgumentList(
+                arguments:
+                    SyntaxFactory.SingletonSeparatedList(
+                        node:
+                            SyntaxFactory.Argument(
+                                expression:
+                                    SyntaxFactory.LiteralExpression(
+                                        kind:
+                                            SyntaxKind.StringLiteralExpression,
+                                        token:
+                                            SyntaxFactory.Literal(value)))));
+        }
+        public static SyntaxTree CreateTree()
+        {
+            MemberAccessExpressionSyntax? ConsoleWriteLine = SyntaxFactory.MemberAccessExpression(
+                kind:
+                    SyntaxKind.SimpleMemberAccessExpression,
+                expression:
+                    SyntaxFactory.IdentifierName(
+                        name: 
+                            nameof(Console).ToString()),
+                name:    
+                    SyntaxFactory.IdentifierName(
+                        name:
+                            nameof(Console.WriteLine)));
+
+            ExpressionStatementSyntax? statement = SyntaxFactory.ExpressionStatement(
+                expression:
+                    SyntaxFactory.InvocationExpression(ConsoleWriteLine)
+                    .WithArgumentList(
+                        argumentList:
+                            CreateSingleArgument("Hello World")));
+
+            MethodDeclarationSyntax? main = SyntaxFactory.MethodDeclaration(
+               returnType:
+                    SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.VoidKeyword)),
+               identifier:
+                    nameof(Program.Main))
+                    .AddModifiers(
+                        items:
+                            SyntaxFactory.Token(
+                                kind:
+                                    SyntaxKind.StaticKeyword))
+                    .AddBodyStatements(
+                                items:
+                                    statement);
+
+            ClassDeclarationSyntax? program = SyntaxFactory.ClassDeclaration(
+                identifier:
+                    nameof(Program))
+                .AddMembers(main);
+
+            NamespaceDeclarationSyntax? rootNameSpace = SyntaxFactory.NamespaceDeclaration(
+                name:
+                    SyntaxFactory.IdentifierName(
+                        name:
+                            nameof(CodeAnalyzer)));
+
+            rootNameSpace = rootNameSpace.AddMembers(
+                items:
+                    program);
+
+            UsingDirectiveSyntax? usingDirective = SyntaxFactory.UsingDirective(
+                name:
+                    SyntaxFactory.ParseName(
+                        text:
+                            nameof(System)));
+
+            SyntaxTrivia comment = SyntaxFactory.Comment("// Generating Code - Do not Edit");
+
+            CompilationUnitSyntax? unit = SyntaxFactory.CompilationUnit()
+                                    .AddUsings(usingDirective)
+                                    .AddMembers(rootNameSpace)
+                                    .WithLeadingTrivia(comment);
+
+            return unit.SyntaxTree;
+
+        }
+
+    }
+}
---
+++ CodingGenerate/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Emit;
+using System.Reflection;
+
+namespace CodeAnalyzer
+{
+    public class Program
+    {
+        
+        //static void BuildAssembly(SyntaxTree syntaxTree, string assemblyName)
+        //{
+        //    List<MetadataReference> references = new List<MetadataReference>() {
+        //        MetadataReference.CreateFromFile(path: Assembly.Load("System.Private.Corelib").Location),
+        //        MetadataReference.CreateFromFile(path: Assembly.Load("System.Runtime").Location),
+        //        MetadataReference.CreateFromFile(path: Assembly.Load("System.Console").Location),
+        //    };
+        //    CSharpCompilation compilation = CSharpCompilation.Create(
+        //        assemblyName: assemblyName,
+        //        syntaxTrees: new[] { syntaxTree },
+        //        references: references,
+        //        options: new CSharpCompilationOptions(OutputKind.ConsoleApplication)
+        //        );
+
+        //    Directory.CreateDirectory("Output");
+
+        //    using FileStream fileStream = new FileStream(Path.Combine("Output" , assemblyName),FileMode.Create);
+
+        //    EmitResult result = compilation.Emit(fileStream);
+
+
+        //    foreach (var dignostic in result.Diagnostics)
+        //    {
+        //        Console.WriteLine($"{dignostic.Id} : {dignostic.GetMessage()}");
+        //    }
+        //}
+
+
+        public static void Main()
+        {
+            SyntaxTree tree = CodeGenerator.CreateTree();
+
+            Console.WriteLine(tree.GetRoot().NormalizeWhitespace().ToFullString());
+
+
+            //BuildAssembly(tree, "MyHelloWorld.dll");
+        }
+    }
+}
